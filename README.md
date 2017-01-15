9a4a0d6b09579a4aa31697ea3bdb572ec97fe4d4
Tim Noise <tim@drkns.net>
2017-01-15 06:46:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Discord<->Telegram
+
+## Installation
+
+- ```git clone http://github.com/dnoiz1/discord-to-telegram.git```
+- ```npm install```
+- ```cp config.json.sample config.json```
+
+# Telegram Important
+- Speak to ```@BotFather``` on telegram
+- Create a new bot with ```/newbot```
+- ```/setjoingroups``` ENABLED
+- ```/setprivacy``` DISABLED
+- Put the bot token into ```config.json```
+
+# Discord Important
+
+- [Read this](https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token)
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/node
+
+"use strict";
+
+const Discord  = require('discord.js')
+  , Telegram   = require('node-telegram-bot-api')
+  , log        = require('winston');
+
+const config = require('./config.json');
+
+log.level = config.loglevel;
+
+const tg = new Telegram(config.telegram.token, { polling: true });
+const dc = new Discord.Client();
+
+// discord messages
+dc.on('ready', function(){
+    log.info('[discord] connected!');
+});
+
+dc.on('message', function(message){
+    log.debug('[discord-%s-#%s]: <%s#%s> %s',
+              message.channel.type, message.channel.name,
+              message.author.username, message.author.discriminator,
+              message.content
+    );
+
+    if (message.channel.type == 'text'
+        && message.channel.id == config.discord.channel_id
+        && message.guild.id == config.discord.guild_id
+        // and the message isnt from ourself...
+        && message.author.id != dc.user.id
+    ) {
+        // <user#1337> what's up
+        var message_out = '<' + message.author.username + '#' + message.author.discriminator + '> ' + message.content;
+        tg.sendMessage(config.telegram.chat_id, message_out);
+    }
+
+});
+
+dc.login(config.discord.token);
+
+// telegram messages
+
+tg.on('message', function(message){
+    log.debug('[telegram-%s]: <%s> %s', message.chat.title, message.from.username, message.text);
+
+    if (message.chat.id != config.telegram.chat_id) return;
+
+    var target = dc.channels.get(config.discord.channel_id);
+
+    if(!target) {
+        log.warn('target channel not found: %s', config.discord.channel_id);
+        return;
+    }
+
+    // <user> what's up
+    var message_out = '<' + message.from.username + '> ' + message.text;
+    target.sendMessage(message_out);
+
+});
